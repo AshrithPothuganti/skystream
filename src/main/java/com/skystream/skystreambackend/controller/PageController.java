83a5b762def94d10eaadf891e66b67007979a0d6
Delta PracticeGitCmd <bunnypothuganti222@gmail.com>
2025-12-15 13:43:10 +0530

    PageController style HomeController  check

--- src/main/java/com/skystream/skystreambackend/controller/PageController.java
+++ src/main/java/com/skystream/skystreambackend/controller/PageController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/page")
+    public String page() {
         return "dashboard";
     }
     
