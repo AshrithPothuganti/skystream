1562f13de4d88c3a2d04d3c367b2e1e6e6349554
Delta PracticeGitCmd <bunnypothuganti222@gmail.com>
2025-12-20 17:33:04 +0530

    HealthController check

--- src/main/java/com/skystream/skystreambackend/controller/HealthController.java
+++ src/main/java/com/skystream/skystreambackend/controller/HealthController.java
@@ -5,11 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-
-    @GetMapping("/")
-    public String home() {
-        return "SkyStream backend running on Render";
-    }
+    
 
     @GetMapping("/health")
     public String health() {
